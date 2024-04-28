48df1fff22966436d88b5e02305548389912d18f
danielb088 <danielbaridk@gmail.com>
2024-04-28 16:09:04 +0300

    collision detection

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import random
 from pygame.locals import *
 
 MOVE_SPEED = 4
-ENEMY_SPEED = 2
-MAX_PLAYERS = 15
+ENEMY_SPEED = 1
+MAX_PLAYERS = 10
 LEFT_BORDER = 0
 RIGHT_BORDER = 360
+
  
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((360,640))
@@ -16,6 +17,8 @@ bg = pygame.image.load("background.png")
 player = pygame.image.load("character.png")
 enemy = pygame.image.load("enemies.png")
 
+ENEMY_SIZE = enemy.get_size()
+PLAYER_SIZE = player.get_size()
 
 player_x = screen.get_width()/2 + 10
 player_y = screen.get_height() - player.get_size()[1]
@@ -31,9 +34,7 @@ def findTopPlayerY():
             min_y=car[1]
     return min_y 
 
-
-
-def generatePalyers():
+def generateEnemies():
     global enemies
     if (findTopPlayerY()> player.get_size()[1]) and (len(enemies)<MAX_PLAYERS):
         x = random.randint(0,1)
@@ -46,15 +47,14 @@ def generatePalyers():
             x = random.randint(0,RIGHT_BORDER)
             enemies.append([x,0])
 
-
-def showPlayers():
+def showEnemies():
     print(enemies)
     global screen
     for player in enemies:
         screen.blit(enemy, (player[0],player[1]))
 
 
-def movePlayers():  #move the enemies on screen
+def moveEnemies():  #move the enemies on screen
     global enemies
     for car in enemies:
         car[1]+= ENEMY_SPEED
@@ -78,6 +78,20 @@ def remove_enemies():
     for player in enemies:
         if player[1] > screen.get_height()-enemy.get_size()[1]:
             enemies.remove(player)
+
+def collision_detect():
+    global enemies
+    global PLAYER_SIZE
+    global ENEMY_SIZE
+    global player_x
+    global player_y
+
+    RECT_PLAYER = Rect(player_x, player_y, PLAYER_SIZE[0], PLAYER_SIZE[1])
+    for enemy in enemies:
+        RECT_ENEMY = Rect(enemy[0], enemy[1], ENEMY_SIZE[0], ENEMY_SIZE[1])
+        collide = pygame.Rect.colliderect(RECT_PLAYER, RECT_ENEMY)
+        if collide:
+            print("you have been hit")
             
 
 pygame.display.set_caption('bullet hell shooter')
@@ -108,14 +122,14 @@ while True:
     #     if event.key == K_DOWN:
     #         player_y += MOVE_SPEED
         
-    screen.blit(bg,(0,0))
-    movePlayers()
-    generatePalyers()
-    showPlayers()
+    screen.blit(bg,(0,0)) 
+    moveEnemies()
+    generateEnemies()
+    showEnemies()
+    collision_detect()
     check_boundaries()
     remove_enemies()
     
     screen.blit(player, (player_x,player_y))
-
 
     pygame.display.update()
